9f52a02334a64b8e335e7e9ae3e4e567f079c741
annatyu <anna.e.tyulkina@gmail.com>
2026-06-18 19:32:02 +0700

    добавлен интерфейс

---
+++ WinFormsConsumable/IConsumableRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsConsumable
+{
+    internal interface IConsumableRepository
+    {
+        // Метод возвращает true, если расходник с таким именем уже есть в БД
+        bool IsNameExists(string name);
+    }
+}
