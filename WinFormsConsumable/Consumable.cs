d7c5db34e153145b813e5d8b480aad69a34277bb
annatyu <anna.e.tyulkina@gmail.com>
2026-06-18 19:42:36 +0700

    изменили модификатор доступа в интерфейсе, добавили конструктор и еще один тест в класс

--- WinFormsConsumable/Consumable.cs
+++ WinFormsConsumable/Consumable.cs
@@ -4,6 +4,13 @@ namespace WinFormsConsumable
 {
     public class ConsumableValidator
     {
+        private readonly IConsumableRepository _repository;
+
+        // Конструктор принимает зависимость (опционально для старых тестов)
+        public ConsumableValidator(IConsumableRepository repository = null)
+        {
+            _repository = repository;
+        }
         public bool CheckConsumableData(string name, string cycleValue, string periodType)
         {
             // 1. Проверка на пустоту имени
@@ -11,20 +18,25 @@ namespace WinFormsConsumable
             {
                 throw new ArgumentException("Название расходника не может быть пустым");
             }
+            // 2. Проверка дубликата имени в БД(Использование зависимости)
+            if (_repository != null && _repository.IsNameExists(name))
+            {
+                throw new InvalidOperationException("Расходник с таким названием уже существует");
+            }
 
-            // 2. Проверка, что цикл является числом
+            // 3. Проверка, что цикл является числом
             if (!int.TryParse(cycleValue, out int parsedCycle))
             {
                 throw new FormatException("Значение цикла должно быть числом");
             }
 
-            // 3. Проверка на положительное значение числового цикла
+            // 4. Проверка на положительное значение числового цикла
             if (parsedCycle <= 0)
             {
                 throw new ArgumentOutOfRangeException("cycleValue", "Цикл замены должен быть больше нуля");
             }
 
-            // 4. Проверка периода
+            // 5. Проверка периода
             if (string.IsNullOrWhiteSpace(periodType))
             {
                 throw new ArgumentException("Не выбран тип периода");
--- WinFormsConsumable/IConsumableRepository.cs
+++ WinFormsConsumable/IConsumableRepository.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace WinFormsConsumable
 {
-    internal interface IConsumableRepository
+    public interface IConsumableRepository
     {
         // Метод возвращает true, если расходник с таким именем уже есть в БД
         bool IsNameExists(string name);
